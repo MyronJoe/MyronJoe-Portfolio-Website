dc5d2a68bc2813b3217b8faf85b94afa5fba3c76
Myron <josephonyekachi77@gmail.com>
2022-10-06 22:03:15 -0800

    updated

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, url_for, redirect
+from forms import RegistrationForm, LoginForm
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '34572773a493b3a41f6a6f251dda0c73'
@@ -11,12 +12,14 @@ def home():
 
 @app.route('/register')
 def register():
-    return render_template('register.html', title='Register')
+    form = RegistrationForm()
+    return render_template('register.html', title='Register', form=form)
 
 
 @app.route('/login')
 def login():
-    return render_template('login.html', title='Register')
+    form = LoginForm()
+    return render_template('login.html', title='Register', form=form)
 
 
 if __name__ == '__main__':
