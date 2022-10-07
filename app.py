1583f55c41fc8c8517e4251b1e5abadc7cbdda6f
Myron <josephonyekachi77@gmail.com>
2022-10-06 20:51:04 -0800

    updated, added the forms.py file for Login and Register

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for, redirect
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = '34572773a493b3a41f6a6f251dda0c73'
 
 
 @app.route('/')
@@ -11,6 +12,11 @@ def home():
 @app.route('/register')
 def register():
     return render_template('register.html', title='Register')
+
+
+@app.route('/login')
+def login():
+    return render_template('login.html', title='Register')
 
 
 if __name__ == '__main__':
