6502b471984fb1d9ef65fbb479663375e846ab07
MyronJoe <josephonyekachi77@gmail.com>
2022-10-09 08:00:49 -0700

    updated

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,11 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '3c33b8874083c78b15cf465c32973cad'
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///portfolio.db"
 db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
 
 from app import routes
--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,5 @@
 from flask import render_template, url_for, redirect, flash
-from app import app
+from app import app, db, bcrypt
 from app.forms import RegistrationForm, LoginForm
 from app.models import User, Post, Message
 
@@ -14,10 +14,13 @@ def home():
 def register():
     form = RegistrationForm()
     if form.validate_on_submit():
-
-        flash(f'Account created for {form.username.data}!', 'success')
+        hashed_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
+        user = User(username=form.username.data, email=form.email.data, password=hashed_password)
+        db.session.add(user)
+        db.session.commit()
+        flash('Your account was created successfully', 'success')
         return redirect(url_for('home'))
-    return render_template('register.html', title='Register', form=form)
+    return render_template('register.html', form=form)
 
 
 @app.route('/login', methods=['GET', 'POST'])
